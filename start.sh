14c017fc9c444176f8b24650814176c8eb659e59
I Made Ocy Darma Putra <ochi@troke.id>
2021-04-22 19:46:18 +0800

    tambahin detach

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Script Start
-smug start api
-smug start avatar
-smug start beatmap-server
-smug start lets
-smug start multi-backend
-smug start multi-frontend
-smug start peppy
-smug start yohane
+smug start api --detach
+smug start avatar --detach
+smug start beatmap-server --detach
+smug start lets --detach
+smug start multi-backend --detach
+smug start multi-frontend --detach
+smug start peppy --detach
+smug start yohane --detach
 echo 'Completed'
