71f5d5abc970eb403cda73a08d6fcafda289e572
Rei_Fan49 <reimu_after_marisa@yahoo.com>
2022-04-21 22:29:35 +0800

    allow variable list of smg mass command

--- start.sh
+++ start.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 # Script Start
-smug start api --detach
-smug start avatar --detach
-smug start beatmap-server --detach
-smug start lets --detach
-smug start multi-backend --detach
-smug start multi-frontend --detach
-smug start peppy --detach
-smug start yohane --detach
-smug start web --detach
-smug start megumin --detach
-smug start network --detach
-smug start gopi --detach
-smug start yohaneirc --detach
-smug start exhentai --detach
-smug start nadeko --detach
+for config in $(smug list); do
+  smug start "$config" --detach
+done
 echo 'Completed'
--- stop.sh
+++ stop.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 # Script stop
-smug stop api
-smug stop avatar
-smug stop beatmap-server
-smug stop lets
-smug stop multi-backend
-smug stop multi-frontend
-smug stop peppy
-smug stop yohane
-smug stop web
-smug stop megumin
-smug stop network
-smug stop gopi
-smug stop yohaneirc
-smug stop exhentai
-smug stop nadeko
+for config in $(smug list); do
+  smug stop "$config"
+done
 echo 'Completed'
