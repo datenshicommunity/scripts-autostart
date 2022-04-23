b6f36bfdfb2f0ec57da34dc570f7ba510432274b
DATENSHI <datenshicommunity@gmail.com>
2022-04-23 10:44:46 +0800

    fix startup minecraft

--- start.sh
+++ start.sh
@@ -6,19 +6,23 @@ done
 sleep 3
 
 # Minecraft start
-# Config
-source config/global.config
+. config/global.config
 
-curl "$mcpanel/api/client/servers/$mcpanelserverid/power" \
-  -H 'Accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -H 'Authorization: Bearer $mcpaneltoken' \
-  -X POST \
-  -b 'pterodactyl_session'='$mcpanelcookies' \
-  -d '{"signal": "start"}'
-
+if [[ -z $mcpanelserverid ]]; then
+  echo "Ignoring MC auto start." >&2
+else
+  curl \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $mcpaneltoken" \
+    -X POST \
+    -b "pterodactyl_session=$mcpanelcookies"  \
+    -d '{"signal": "start"}' \
+    "https://panel.datenshi.pw/api/client/servers/$mcpanelserverid/power"
+fi
 sleep 3
 
+# Send to Discord
 curl -X POST --data '{"content": "`[SERVER]` All-server has been started! <@176829603321610240>"}' --header "Content-Type:application/json" $webhookDC
 
 echo 'Completed'
--- stop.sh
+++ stop.sh
@@ -3,22 +3,26 @@
 for config in $(smug list); do
   smug stop "$config"
 done
+sleep 3
+
 # Minecraft stop
-# Config
-source config/global.config
+. config/global.config
 
+if [[ -z $mcpanelserverid ]]; then
+  echo "Ignoring MC auto stop." >&2
+else
+  curl \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $mcpaneltoken" \
+    -X POST \
+    -b "pterodactyl_session=$mcpanelcookies"  \
+    -d '{"signal": "stop"}' \
+    "https://panel.datenshi.pw/api/client/servers/$mcpanelserverid/power"
+fi
 sleep 3
 
-curl "$mcpanel/api/client/servers/$mcpanelserverid/power" \
-  -H 'Accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -H 'Authorization: Bearer $mcpaneltoken' \
-  -X POST \
-  -b 'pterodactyl_session'='$mcpanelcookies' \
-  -d '{"signal": "stop"}'
-
-sleep 3
-
+# Send to Discord
 curl -X POST --data '{"content": "`[SERVER]` All-server has been stopped! <@176829603321610240>"}' --header "Content-Type:application/json" $webhookDC
 
 echo 'Completed'
