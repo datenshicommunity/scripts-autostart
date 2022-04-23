f101e9fdab43854228df1f21fefe2ece2776a83f
troke12 <jame_glove@yahoo.com>
2022-04-23 09:19:27 +0800

    update auto-start

--- start.sh
+++ start.sh
@@ -3,4 +3,18 @@
 for config in $(smug list); do
   smug start "$config" --detach
 done
+sleep 3
+
+# Minecraft start
+# Config
+source minecraft.config
+
+curl "$mcpanel/api/client/servers/$mcpanelserverid/power" \
+  -H 'Accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer $mcpaneltoken' \
+  -X POST \
+  -b 'pterodactyl_session'='$mcpanelcookies' \
+  -d '{"signal": "start"}'
+
 echo 'Completed'
--- stop.sh
+++ stop.sh
@@ -3,4 +3,17 @@
 for config in $(smug list); do
   smug stop "$config"
 done
+# Minecraft stop
+# Config
+source minecraft.config
+
+sleep 3
+
+curl "$mcpanel/api/client/servers/$mcpanelserverid/power" \
+  -H 'Accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer $mcpaneltoken' \
+  -X POST \
+  -b 'pterodactyl_session'='$mcpanelcookies' \
+  -d '{"signal": "stop"}'
 echo 'Completed'
